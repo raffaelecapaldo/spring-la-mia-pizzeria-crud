cd913c3718f599e4e21ff6dd42dce8d91769e613
raffaelecapaldo <raffaelecpl@gmail.com>
2023-10-04 15:06:47 +0200

    Completed pizza entity

--- src/main/java/org/java/app/business/pojo/Pizza.java
+++ src/main/java/org/java/app/business/pojo/Pizza.java
@@ -21,6 +21,58 @@ public class Pizza {
 	private String imageUrl;
 	
 	@Column(nullable = false)
-	private BigDecimal price;
+	private int price;
+	
+	public Pizza() { }
+	public Pizza(String name, String description, String imageUrl, int price) {
+		setName(name);
+		setDescription(description);
+		setImageUrl(imageUrl);
+		setPrice(price);
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) throws Exception {
+		if (price < 0) 
+			throw new Exception("Price can't be less than zero");
+			
+		this.price = price*100;
+	}
+	
+	@Override
+	public String toString() {
+		return "[ID]: " + getId() 
+				+ "[Name]: " + getName()
+				+ "[Description]" + getDescription()
+				+ "[Image url]: " + getImageUrl()
+				+ "[Price] : " + getPrice()/100;
+	}
+	
+	
 	
 }
