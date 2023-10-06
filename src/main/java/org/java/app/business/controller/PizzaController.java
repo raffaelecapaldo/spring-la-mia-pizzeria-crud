8654454cafc019e96c17f035d95c46960f023181
raffaelecapaldo <raffaelecpl@gmail.com>
2023-10-06 13:02:03 +0200

    Added storePizza (no validations implemented)

--- src/main/java/org/java/app/business/controller/PizzaController.java
+++ src/main/java/org/java/app/business/controller/PizzaController.java
@@ -7,10 +7,15 @@ import org.java.app.business.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -49,6 +54,11 @@ public class PizzaController {
 		return "pizza/pizza-create";
 	}
 	
-	
+	@PostMapping("/create")
+	public String storePizza(@Valid @ModelAttribute Pizza pizza, BindingResult bindingResult, Model model) {
+		pizzaService.save(pizza);
+		
+		return "redirect:/pizzas";
+	}
 	
 }
